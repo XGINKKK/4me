f66cca8024edadb0d40937b1dcb499eb80b648f0
XGINKKK <skolashard123@gmail.com>
2025-09-24 16:35:27 -0300

    Corrigir erro de sintaxe substituindo const por var

--- extract-zip.js
+++ extract-zip.js
@@ -1,8 +1,8 @@
  const fs = require('fs');
  const path = require('path');
  
--const zipPath = 'saveweb2zip-com-9000-firebase-studio-1758720228337-cluster-57i2ylwve5fskth4xb2kui2ow2-cloudworkstations-dev.zip';
-+const zipPath = 'saveweb2zip-com-9000-firebase-studio-1758720228337-cluster-57i2ylwve5fskth4xb2kui2ow2-cloudworkstations-dev (1).zip';
+var zipPath = 'saveweb2zip-com-9000-firebase-studio-1758720228337-cluster-57i2ylwve5fskth4xb2kui2ow2-cloudworkstations-dev (1).zip';
+var extractPath = './extracted';
  const extractPath = './extracted';
  
  // Create extraction directory if it doesn't exist
