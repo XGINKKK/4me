d285d80747265437e396f1709ff2159ec28e800b
XGINKKK <skolashard123@gmail.com>
2025-09-24 16:35:45 -0300

    Corrigir declaração duplicada de extractPath

--- extract-zip.js
+++ extract-zip.js
@@ -1,8 +1,7 @@
  const fs = require('fs');
  const path = require('path');
  
-var zipPath = 'saveweb2zip-com-9000-firebase-studio-1758720228337-cluster-57i2ylwve5fskth4xb2kui2ow2-cloudworkstations-dev (1).zip';
-var extractPath = './extracted';
- const extractPath = './extracted';
+const zipPath = 'saveweb2zip-com-9000-firebase-studio-1758720228337-cluster-57i2ylwve5fskth4xb2kui2ow2-cloudworkstations-dev (1).zip';
+const extractPath = './extracted';
  
  // Create extraction directory if it doesn't exist
